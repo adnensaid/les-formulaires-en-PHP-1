6aa363b107c61b53c3651e0357a0e970b01ac14f
adnensaid <adnensaid83@gmail.com>
2021-12-07 16:40:20 +0100

    version 1.0.0

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<form  action="ma-page-de-traitement.php"  method="POST">
+  <div>
+    <label  for="nom">Nom :</label>
+    <input  type="text"  id="nom"  name="nom">
+  </div>
+  <div>
+    <label  for="prenom">Prenom :</label>
+    <input  type="text"  id="prenom"  name="prenom">
+  </div>
+  <div>
+    <label  for="email">Email :</label>
+    <input  type="text"  id="email"  name="email">
+  </div>  
+  <div>
+    <label  for="tel">Tel :</label>
+    <input  type="text"  id="email"  name="tel">
+  </div>    
+  <div>
+    <label  for="sujet">Sujet :</label>
+    <input  type="text"  id="sujet"  name="sujet">
+  </div>
+  <div>
+    <label  for="message">Message :</label>
+    <textarea  id="message"  name="user_message"></textarea>
+  </div>
+  <div  class="button">
+    <button  type="submit">Envoyer votre message</button>
+  </div>
+</form>
---
+++ public/ma-page-de-traitement.php
@@ -0,0 +1,4 @@
+<div class="response">
+  <p>Merci <?= $_GET["prenom"]." ".$_GET["nom"] ?> de nous avoir contacté à propos de <?= $_GET["sujet"] ?>.</p>
+  <p> Un de nos conseiller vous contactera soit à l'adresse <?= $_GET["email"] ?> ou par téléphone au <?= $_GET["tel"] ?> </p>
+</div>
